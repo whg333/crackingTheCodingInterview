f25b6984caad2648108395add1124430fdd6364a
whg333 <whg3.3.3@163.com>
2016-04-17 01:27:01 +0800

    update

--- src/main/java/com/why/hoolai/StrigParser.java
+++ src/main/java/com/why/hoolai/StrigParser.java
@@ -4,24 +4,76 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.Stack;
 
 public class StrigParser {
 
     public static void main(String[] args) {
         String s = "(A(B)(C(D)(E))F)";
-        ParenthesisContent pc = new ParenthesisContent(s);
-        System.out.println(pc.getContent(1));
-        System.out.println(pc.getContent(2));
-        System.out.println(pc.getContent(3));
-        System.out.println(pc.getContent(4));
-        System.out.println(pc.getContent(5));
+//        ParenthesisContent pc = new ParenthesisContent(s);
+//        System.out.println(pc.getContent(1));
+//        System.out.println(pc.getContent(2));
+//        System.out.println(pc.getContent(3));
+//        System.out.println(pc.getContent(4));
+//        System.out.println(pc.getContent(5));
+        
+        ParenthesisContentOnce pco = new ParenthesisContentOnce();
+        System.out.println(pco.getContent(s, 1));
+        System.out.println(pco.getContent(s, 2));
+        System.out.println(pco.getContent(s, 3));
+        System.out.println(pco.getContent(s, 4));
+        System.out.println(pco.getContent(s, 5));
     }
     
+    /** 可以不必全部遍历完成，当找到指定index后直接返回对应栈中的内容 */
+    private static final class ParenthesisContentOnce{
+        
+        public String getContent(String s, int index){
+            char[] charArray = s.toCharArray();
+            int leftParenthesis = 0, rightParenthesis = 0;
+            LinkedList<IndexContent> contentQueue = new LinkedList<IndexContent>();
+            
+            for(int i=0;i<charArray.length;i++){
+                char currentChar = charArray[i];
+                if(currentChar == '('){
+                    leftParenthesis++;
+                    collectContent(contentQueue, currentChar);
+                    contentQueue.addFirst(new IndexContent(leftParenthesis));
+                }else if(currentChar == ')'){
+                    rightParenthesis++;
+                    IndexContent indexContent = contentQueue.removeFirst();
+                    if(index == indexContent.index){
+                        //System.out.println("Found!");
+                        return indexContent.content.toString();
+                    }
+                    collectContent(contentQueue, currentChar);
+                }else{
+                    collectContent(contentQueue, currentChar);
+                }
+                
+            }
+            
+            if(leftParenthesis != rightParenthesis){
+                throw new IllegalArgumentException("括号没有成对出现！");
+            }
+            
+            return "No Found!";
+        }
+        
+        /** 针对所有在栈内的元素都做收集内容的操作 */
+        private void collectContent(LinkedList<IndexContent> contentQueue, char currentChar){
+            Iterator<IndexContent> it = contentQueue.iterator();
+            while(it.hasNext()){
+                it.next().collect(currentChar);
+            }
+        }
+        
+    }
+    
+    /** 全部遍历完成后构造的一个index --> content的键值映射，便于下次根据index直接getContent出来 */
     private static final class ParenthesisContent{
     	
     	private final Map<Integer, StringBuilder> contentMap = new HashMap<Integer, StringBuilder>();
-    	private final LinkedList<StringBuilder> contentQueue = new LinkedList<StringBuilder>();
+    	private final LinkedList<IndexContent> contentQueue = new LinkedList<IndexContent>();
     	
     	public ParenthesisContent(String s){
     		parse(s);
@@ -31,19 +83,19 @@ public class StrigParser {
     		char[] charArray = s.toCharArray();
             int leftParenthesis = 0, rightParenthesis = 0;
             
-            Stack<Integer> countStack = new Stack<Integer>();
             for(int i=0;i<charArray.length;i++){
-                if(charArray[i] == '('){
+                char currentChar = charArray[i];
+                if(currentChar == '('){
                 	leftParenthesis++;
-                	collectContent(charArray[i]);
-                    contentQueue.addFirst(new StringBuilder());
-                    countStack.push(leftParenthesis);
-                }else if(charArray[i] == ')'){
+                	collectContent(currentChar);
+                    contentQueue.addFirst(new IndexContent(leftParenthesis));
+                }else if(currentChar == ')'){
                 	rightParenthesis++;
-                	contentMap.put(countStack.pop(), contentQueue.removeFirst());
-                	collectContent(charArray[i]);
+                	IndexContent indexContent = contentQueue.removeFirst();
+                	contentMap.put(indexContent.index, indexContent.content);
+                	collectContent(currentChar);
                 }else{
-                	collectContent(charArray[i]);
+                	collectContent(currentChar);
                 }
                 
             }
@@ -53,11 +105,11 @@ public class StrigParser {
             }
     	}
     	
+    	/** 针对所有在栈内的元素都做收集内容的操作 */
     	private void collectContent(char currentChar){
-    		Iterator<StringBuilder> it = contentQueue.iterator();
+    		Iterator<IndexContent> it = contentQueue.iterator();
         	while(it.hasNext()){
-        		StringBuilder sb = it.next();
-        		sb.append(currentChar);
+        		it.next().collect(currentChar);
         	}
     	}
     	
@@ -65,6 +117,17 @@ public class StrigParser {
     		return contentMap.get(index).toString();
     	}
     	
+    }
+    
+    private static class IndexContent{
+        private final int index;
+        private final StringBuilder content = new StringBuilder();
+        public IndexContent(int index) {
+            this.index = index;
+        }
+        public void collect(char c){
+            content.append(c);
+        }
     }
     
 }
